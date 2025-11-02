78b177e16ec421ab72614d5c8c6fd8a4c7601d9f
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-03 00:55:20 +0600

    14.2 done

--- retuen+peramiter.c
+++ retuen+peramiter.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
+int sub(int num1, int num2)
+{
+    int ans = num1 - num2;
+    return ans;
+}
 int sum(int num1, int num2)
 {
     int ans = num1 + num2;
@@ -7,7 +12,9 @@ int sum(int num1, int num2)
 }
 int main()
 {
-    int val = sum(10,5);
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int val = sub(a,b);
     int val2 =sum(100,200);
     printf("%d %d",val,val2);
     return 0;
