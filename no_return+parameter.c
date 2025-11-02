de9017175688bcfe4c41698089ba8dbaa42b6597
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-03 01:16:24 +0600

    no return + parameter

---
+++ no_return+parameter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void sum(int num1, int num2)
+{
+    int ans = num1 + num2;
+    printf("%d", ans);
+    return;
+}
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    sum(a,b);
+
+    return 0;
+}
