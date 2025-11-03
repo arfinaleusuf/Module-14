482857eac66e9bb3fefeb1933b01805307401187
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-03 19:04:24 +0600

    scope

--- no_return+parameter.c
+++ no_return+parameter.c
@@ -10,7 +10,7 @@ int main()
 {
     int a, b;
     scanf("%d %d", &a, &b);
-    sum(a,b);
+    sum(a, b);
 
     return 0;
 }
---
+++ scope.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int sum()
+{
+    int a = 20;
+    printf("%d\n", a);
+}
+int main()
+{
+    int a = 10;
+    printf("%d\n", a);
+    sum();
+    return 0;
+}
