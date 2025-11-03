b5fc7e23b80586878434f6655f02084ba293bb93
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-03 19:25:03 +0600

    useful math function

--- useful_marh_function.c
+++ useful_marh_function.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    
+    int ans = pow(3,2);
+    printf("%d",ans);
     return 0;
 }
+
+/* #include<stdio.h> add kore nite hobe
+ceil() --> man bariye purno shongkha banay
+floor() --> man komiye purno shongkha baniye dey
+round() --> normal vabe purno shongkha baniye dey
+sqrt() --> square root kore
+pow() --> power kore (5,2) 5^2 eita bojhay
+abs() --> modulas kore 
+*/
